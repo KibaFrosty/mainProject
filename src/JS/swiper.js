14dd809399addb95176ad34b85640bd072fd1e51
KibaFrosty <kirillnovakovskii98@gmail.com>
2022-08-01 17:14:40 +0300

    add adaptiv

--- src/JS/swiper.js
+++ src/JS/swiper.js
@@ -2,6 +2,9 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+
   
     // If we need pagination
     pagination: {
@@ -13,4 +16,18 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      100: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+    }
   });
